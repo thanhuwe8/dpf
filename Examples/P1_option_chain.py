50610098387fdd4931c3ffe3d29ca32bf4574dcd
thanhuwe8 <45818908+thanhuwe8@users.noreply.github.com>
2022-08-17 12:42:38 +0200

    Initial commit

---
+++ Examples/P1_option_chain.py
@@ -0,0 +1,61 @@
+from dpf.scripting import BSVanilla
+
+import pandas as pd
+import numpy as np
+from yahoo_fin import options
+import datetime
+
+
+tesla = options.get_options_chain("TSLA")
+
+tesla_call = tesla['calls']
+tesla_put = tesla['puts']
+
+tesla_call.columns
+
+
+# datetime manipulation
+expiration_raw = options.get_expiration_dates("TSLA")
+expiration_list = [datetime.datetime.strptime(date,  "%B %d, %Y") for date in expiration_raw]
+
+start = datetime.datetime.today().date()
+end = expiration_list[0].date()
+
+days = np.busday_count(start, end)
+
+
+time_to_maturity = []
+for day in expiration_list:
+    end = day.date()
+    days = np.busday_count(start, end)
+    time_to_maturity.append(days)
+
+
+target_date = expiration_raw[7]
+options_data = options.get_options_chain("TSLA", target_date)
+options_data['calls'].sort_values(by='Open Interest')
+options_data['calls']['Strike'].unique()
+
+Strike_list = list(map(float, range(600,1400,50)))
+
+time_to_maturity[8]
+
+
+target_date = expiration_raw[8]
+options_data = options.get_options_chain("TSLA", target_date)
+options_data['calls'].sort_values(by='Open Interest')
+options_data['calls']['Last Price']
+
+call_option_3m = options_data['calls']
+call_option_3m = call_option_3m[call_option_3m['Strike'].isin(Strike_list)][['Strike', 'Last Price', 'Implied Volatility']]
+
+put_option_3m = options_data['puts']
+put_option_3m = put_option_3m[put_option_3m['Strike'].isin(Strike_list)][['Strike', 'Last Price', 'Implied Volatility']]
+
+call_option_3m
+put_option_3m
+
+
+put_option_3m.plot(x='Strike', y='Implied Volatility')
+pd.concat([put_option_3m, call_option_3m], axis=1)
+
---
+++ dpf/tests/test_scripting_iv.py
@@ -0,0 +1,27 @@
+from dpf.scripting import BSVanilla
+import numpy as np
+
+'''
+This is used to test implied volatility function
+'''
+
+#? Test 1: Dan Stefanica's example
+type1 = "CALL"
+type2 = "PUT"
+S0 = 80
+K = 100
+sigma = 0.2
+t = 0.0
+T = 1.0
+r = 0.05
+observed_price = 7
+
+
+#Put_object = BSVanilla.BlackScholes(type2,S0,K,sigma,t,T,r)
+# implied_volatility = BSVanilla.BlackScholes.newton_raphson_iv(Call_object, observed_price, max_counter=10)
+#implied_volatility2 = BSVanilla.BlackScholes.newton_raphson_iv(Put_object, observed_price, max_counter=10)
+
+# implied_volatility['NewIV']
+#implied_volatility2['NewIV']
+
+
